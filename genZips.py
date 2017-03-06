a5f59e1f93a7bcfa38cceae96ca83354367cb124
Natasha D'Silva <natashad@natashas-mbp.torolab.ibm.com>
2017-03-06 15:16:48 -0500

    remove git interaction from server

--- genZips.py
+++ genZips.py
@@ -15,17 +15,18 @@ def zipdir(path, ziph):
             ziph.write(os.path.join(root, file))
 
 def zip_cur_directory():
+    num_zipped = 0
     sub_dirs = glob("*/")
     for category in sub_dirs:
         #every top level folder is a category
         os.chdir(category)
         for dir in glob('*/'): #if it doesn't, check its sub directories
-            print dir
             project_name = dir[:-1]
             zip_name = project_name +".zip"
             zipf = ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED)
             #print "Creating " + zip_name  + " in " + os.getcwd()
             zipdir(dir, zipf)
+
             zipf.close()
             #print "moving " + zip_name + "to " + dir
             #print "cur dir "+os.getcwd()
@@ -34,11 +35,13 @@ def zip_cur_directory():
             #except (OSError,RuntimeError) as e:
             #    print "files"
             shutil.move(zip_name, dir)
+            num_zipped = num_zipped + 1
         os.chdir("..")
+    print "Done...Zipped " + str(num_zipped) + " Directories"
 if __name__ == '__main__':
     if (len(sys.argv) == 1):
         target_dir = os.getcwd()
     else:
         target_dir = sys.argv[1]
     os.chdir(target_dir)
-    zip_cur_directory(target_dir)
+    zip_cur_directory()
--- server.py
+++ server.py
@@ -18,28 +18,7 @@ except ImportError:
 #This way, we can generate the latest zips on demand.
 #ideally, we would run this script every time the samples repoitory was changed.
 
-def clone():
-    os.chdir("static")
-    print "Cloning samples repo"
-    if not (os.path.exists("samples")):
-        try:
-           ret = check_output(["git","clone","https://github.com/IBMStreams/samples.git"])
-           os.chdir("samples")
-        except CalledProcessError as e:
-            print "Error cloning samples: " + e.output
-            return -1;
 
-    else:
-        #already exists, pull
-        os.chdir("samples")
-        call(["git","pull"])
-
-    #generate zips
-    #TO-DO: check for latest catalog and update
-    genZips.zip_cur_directory()
-    print ("Generated zips...ready to go")
-    os.chdir("..")
-    return 0
 
 def start_server(root_dir):
     # Read port selected by the cloud for our application
@@ -55,7 +34,4 @@ def start_server(root_dir):
     httpd.server_close()
 
 root_dir = os.getcwd()
-if clone() == 0:
-    start_server(root_dir)
-else:
-    print "Cannot start server..Check logs."
+start_server(root_dir)
